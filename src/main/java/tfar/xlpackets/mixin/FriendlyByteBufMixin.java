51920ed9d5b5f2129108e3d84690f23d09a862d0
Gu_ZT <administration@ztxy666.cn>
2022-10-08 14:58:52 +0800

    1.18 port
    Update xlpackets.mixins.json

---
+++ src/main/java/tfar/xlpackets/mixin/FriendlyByteBufMixin.java
@@ -0,0 +1,14 @@
+package tfar.xlpackets.mixin;
+
+import net.minecraft.network.FriendlyByteBuf;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(FriendlyByteBuf.class)
+public class FriendlyByteBufMixin {
+    @ModifyConstant(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", constant = @Constant(longValue = 2097152L))
+    private long xlPackets(long old) {
+        return 2147483647L;
+    }
+}
